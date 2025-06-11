cd76a2b6a777292cfbae711454ac984e367ee659
Maria Barreiro <mariaeobarreiro@gmail.com>
2025-06-11 13:58:03 +0100

    parsing for the render of the grass

--- Src/Render/render_map.c
+++ Src/Render/render_map.c
@@ -13,3 +13,39 @@
 #include "../../Includes/so_long.h"
 
 void	render_map(t_game *game)
+{
+
+}
+
+void	put_image(t_game *game, void *img, int tile_x, int tile_y)
+{
+	int	pixel_x;
+	int	pixel_y;
+
+	pixel_x = tile_x * SIZE;
+	pixel_y = tile_y * SIZE;
+	mlx_put_image_to_window(game->mlx_ptr, game->mlx_win_ptr, img, tile_x, tile_y);
+}
+
+void	*select_grass_combinations(t_game *game, int x, int y)
+{
+	char	**map;
+	int		up;
+	int		down;
+	int		left;
+	int		right;
+
+	map = game->map;
+	up = y > 0 && map[y - 1][x] == '1';
+	down = y < (game->height - 1) && map[y + 1][x] == 1;
+	left = x > 0 && map[x - 1][y] == '1';
+	right = x < (game->width - 1) && map[y][x + 1] == 1;
+	if (up == 1 && left == 1)
+		return (game->textures.grass_b_lft_up);
+	else if (down == 1 && left == 1)
+		return (game->textures.grass_b_lft_dwn);
+	else if (right == 1 && up == 1)
+		return (game->textures.grass_b_rght_up);
+	else if (right == 1 && down == 1)
+		return (game->textures.grass_b_rght_dwn);
+}
---
+++ Src/Render/render_textures.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render_textures.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mda-enca <mda-enca@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/11 13:43:41 by mda-enca          #+#    #+#             */
+/*   Updated: 2025/06/11 13:44:01 by mda-enca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../Includes/so_long.h"
+
+void	*select_grass_combinations(t_game *game, int x, int y)
+{
+	char	**map;
+	int		up;
+	int		down;
+	int		left;
+	int		right;
+
+	map = game->map;
+	up = y > 0 && map[y - 1][x] == '1';
+	down = y < (game->height - 1) && map[y + 1][x] == 1;
+	left = x > 0 && map[x - 1][y] == '1';
+	right = x < (game->width - 1) && map[y][x + 1] == 1;
+	if (up == 1 && left == 1 && down == 0 && right == 0)
+		return (game->textures.grass_b_lft_up);
+	else if (down == 1 && left == 1 && right == 0 && up == 0)
+		return (game->textures.grass_b_lft_dwn);
+	else if (right == 1 && up == 1 && left == 0 && down == 0)
+		return (game->textures.grass_b_rght_up);
+	else if (right == 1 && down == 1 && up == 0 && left == 0)
+		return (game->textures.grass_b_rght_dwn);
+	return (NULL);
+}
+
+void	*select_grass(t_game *game, int x, int y)
+{
+	char	**map;
+	int		up;
+	int		down;
+	int		left;
+	int		right;
+
+	map = game->map;
+	up = y > 0 && map[y - 1][x] == '1';
+	down = y < (game->height - 1) && map[y + 1][x] == 1;
+	left = x > 0 && map[x - 1][y] == '1';
+	right = x < (game->width - 1) && map[y][x + 1] == 1;
+	if (up == 0 && left == 0 && down == 0 && right == 0)
+		return (game->textures.grass);
+	else if (up == 1 && down == 0 && left == 0 && right == 0)
+		return (game->textures.grass_b_up);
+	else if (down == 1 && up == 0 && left == 0 && right == 0)
+		return (game->textures.grass_b_dwn);
+	else if (right == 1 && up == 0 && left == 0 && down == 0)
+		return (game->textures.grass_b_rght);
+	else if (left == 1 && up == 0 && down == 0 && right == 0)
+		return (game->textures.grass_b_lft);
+	return (NULL);
+}
+
+void	*select_water_combinations(t_game *game, int x, int y)
+{
+}
